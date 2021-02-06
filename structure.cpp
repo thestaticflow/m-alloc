71d6d2e23aa9021b4cea841569f491c815eac7be
mohamed <37941160+thestaticflow@users.noreply.github.com>
2021-02-06 21:45:55 +0300

    Create structure.cpp

---
+++ structure.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctype.h>
+#include <string>
+
+//written by m-s3c
+//Friday Feb 6 2021
+
+struct E {
+                                                char text[40];
+                                                int digits;
+}E;
+
+int *ptomem;
+
+struct* E ptomem;
+
+ptomem = (int*) malloc(sizeof(stuct E));
+
+if(ptomem == NULL){
+                                         std::cerr<<"Error"<<endl;
+  
+}else{
+
+//In case we successifully allocated some memory
+
+                                        scanf("%d", &ptomem->digits);
+
+                                        printf("%d", &ptomem->digits);
+
+}
